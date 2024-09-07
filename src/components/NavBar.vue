915b78da536b733f12fd6d9005b8b70f158ecea4
João Barreto <Joo Barreto>
2024-09-07 00:50:53 -0300

    feat: adiciona bootstrap e fontawesome

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,12 +1,26 @@
-<script>
+<script setup lang="ts">
 </script>
 
 <template>
-    <nav class="">
-        <div><i></i></div>
-        <h1>My Board</h1>
-        <a>
-
-        </a>
+    <nav class="nav app-nav-setting py-2 px-1">
+      <div class="container-fluid">
+        <div class="navbar-brand">
+          <font-awesome-icon :icon="['fa-solid', 'bars']" />
+          My Board
+        </div>
+      </div>
     </nav>
 </template>
+
+<style setup>
+.nav.app-nav-setting {
+  background: #04AA6D;
+}
+.app-nav-setting > .container-fluid > .navbar-brand {
+  color: azure;
+  font-weight: 500;
+  font-size: calc(1.075rem + 1.5vw);
+  font-family: 'Open Sans',sans-serif;
+  text-transform: uppercase;
+}
+</style>
